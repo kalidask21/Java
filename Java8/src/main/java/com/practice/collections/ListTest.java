0e2049625db8f99488020cda11158bb808817e55
Kalidas <kalidas@github.com>
2018-01-31 22:00:38 -0800

    Test
    Test

---
+++ Java8/src/main/java/com/practice/collections/ListTest.java
@@ -0,0 +1,28 @@
+package com.practice.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+	
+	private static List<String> countriesList = new ArrayList<String>();
+	
+	
+	static {
+		countriesList.add("USA");
+		countriesList.add("Mexico");
+		countriesList.add("Spain");
+		countriesList.add("India");
+		countriesList.add("England");
+		countriesList.add("Germany");
+		countriesList.add("Russia");	
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
